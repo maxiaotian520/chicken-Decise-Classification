737c35e52c6d4a92f234172aa644ef88c9fc786e
maxiaotian520 <nxdemxt@gmail.com>
2023-07-02 22:22:34 -0700

    data ingestion completed

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+# CONFIG_FILE_PATH = Path("config/config.yaml")
+# CONFIG_FILE_PATH = Path(__file__).resolve().parent / "config" / "config.yaml"
+CONFIG_FILE_PATH = Path("D:\projects\chicken-Decise-Classification\config\config.yaml").resolve()
+PARAMS_FILE_PATH = Path("params.yaml").resolve()
